64f88ce597ee54630b00db192122c54e4e691201
Steve Tu <stevetu11@yahoo.com>
2020-10-31 22:35:55 -0800

    Adjusted the App theme colors and made initial App navigation layout

--- app/src/main/java/com/steve1316/granblueautomation/HomeFragment.kt
+++ app/src/main/java/com/steve1316/granblueautomation/HomeFragment.kt
@@ -3,10 +3,30 @@ package com.steve1316.granblueautomation
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
 
 class HomeFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        // Inflate the preferences to the view.
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        // Attach onClick listeners to travel to the appropriate fragments for each preference.
+        findPreference<Preference>("nav_raid_planner")?.let {
+            it.setOnPreferenceClickListener {
+                val action = HomeFragmentDirections.actionHomeFragmentToRaidPlannerFragment()
+                findNavController().navigate(action)
+                true
+            }
+        }
+        findPreference<Preference>("nav_settings")?.let {
+            it.setOnPreferenceClickListener {
+                val action = HomeFragmentDirections.actionHomeFragmentToSettingsFragment()
+                findNavController().navigate(action)
+                true
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/steve1316/granblueautomation/RaidPlannerFragment.kt
+++ app/src/main/java/com/steve1316/granblueautomation/RaidPlannerFragment.kt
@@ -5,19 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class RaidPlannerFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_raid_planner, container, false)
     }
 }
