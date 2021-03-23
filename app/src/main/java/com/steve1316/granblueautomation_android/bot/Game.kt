11dc548f06783d86808edf0945695db7f88e56a4
Steve Tu <stevetu11@yahoo.com>
2021-03-23 11:34:33 -0700

    Separated Combat Mode logic and its helper functions into its own file.

--- app/src/main/java/com/steve1316/granblueautomation_android/bot/Game.kt
+++ app/src/main/java/com/steve1316/granblueautomation_android/bot/Game.kt
@@ -154,13 +154,6 @@ class Game(myContext: Context) {
 	}
 	
 	/**
-	 * Checks if the Party wiped during Combat Mode. Updates the retreat flag if so.
-	 */
-	private fun partyWipeCheck() {
-		TODO("not yet implemented")
-	}
-	
-	/**
 	 * Checks for CAPTCHA right after selecting a Summon. If detected, alert the user and stop the bot.
 	 */
 	fun checkForCAPTCHA() {
@@ -195,22 +188,6 @@ class Game(myContext: Context) {
 	 * @return True if the mission was successfully started. False otherwise.
 	 */
 	private fun selectPartyAndStartMission(groupNumber: Int, partyNumber: Int, tries: Int = 3): Boolean {
-		TODO("not yet implemented")
-	}
-	
-	/**
-	 * Find the total number of characters ready to Charge Attack.
-	 *
-	 * @return Total number of image matches found for charge attacks.
-	 */
-	private fun findChargeAttacks(): Int {
-		TODO("not yet implemented")
-	}
-	
-	/**
-	 * Checks if there are any dialog popups during Combat Mode from either Lyria or Vyrn and close them.
-	 */
-	private fun findCombatDialog() {
 		TODO("not yet implemented")
 	}
 	
@@ -270,25 +247,6 @@ class Game(myContext: Context) {
 	}
 	
 	/**
-	 * Selects the portrait of the specified character during Combat Mode.
-	 *
-	 * @param characterNumber The character that needs to be selected.
-	 */
-	private fun selectCharacter(characterNumber: Int) {
-		TODO("not yet implemented")
-	}
-	
-	/**
-	 * Activate the specified skill for the already selected character.
-	 *
-	 * @param characterNumber The character whose skill needs to be used.
-	 * @param skillNumber The skill that needs to be used.
-	 */
-	private fun useCharacterSkill(characterNumber: Int, skillNumber: Int) {
-		TODO("not yet implemented")
-	}
-	
-	/**
 	 * Detect any dropped loot from the Loot Collected screen while clicking away any dialog popups.
 	 *
 	 * @param isPendingBattle Skip the incrementation of runs attempted if this was a Pending Battle. Defaults to false.
@@ -320,47 +278,6 @@ class Game(myContext: Context) {
 	 * @return True if Dimensional Halo was detected and successfully completed. False otherwise.
 	 */
 	private fun checkDimensionalHalo(): Boolean {
-		TODO("not yet implemented")
-	}
-	
-	/**
-	 * Wait for a maximum of 20 seconds until the bot sees either the "Attack" or the "Next" button before starting a new turn.
-	 */
-	private fun waitForAttack() {
-		TODO("not yet implemented")
-	}
-	
-	/**
-	 * Uses the specified healing item during Combat Mode with an optional target if the item requires one.
-	 *
-	 * @param command The command for the healing item to use.
-	 * @param target The character target for the item. This depends on what item it is. Defaults to 0.
-	 */
-	private fun useCombatHealingItem(command: String, target: Int = 0) {
-		TODO("not yet implemented")
-	}
-	
-	/**
-	 * Request backup during Combat mode for this Raid.
-	 */
-	private fun requestBackup() {
-		TODO("not yet implemented")
-	}
-	
-	/**
-	 * Request backup during Combat mode for this Raid by using the Twitter feature.
-	 */
-	private fun tweetBackup() {
-		TODO("not yet implemented")
-	}
-	
-	/**
-	 * Start Combat Mode with the provided combat script.
-	 *
-	 * @param combatScript ArrayList of all the lines in the text file.
-	 * @return True if Combat Mode ended successfully. False otherwise if the Party wiped or backed out without retreating.
-	 */
-	private fun startCombatMode(combatScript: ArrayList<String>): Boolean {
 		TODO("not yet implemented")
 	}
 	
