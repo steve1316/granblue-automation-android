e96f0204460e2d810f295225051ac6d093f1c512
Steve Tu <stevetu11@yahoo.com>
2021-03-26 14:31:30 -0700

    Implemented the entirety of Combat Mode logic.

--- app/src/main/java/com/steve1316/granblueautomation_android/bot/CombatMode.kt
+++ app/src/main/java/com/steve1316/granblueautomation_android/bot/CombatMode.kt
@@ -465,6 +465,395 @@ class CombatMode(private val game: Game) {
 	 * @return True if Combat Mode ended successfully. False otherwise if the Party wiped or backed out without retreating.
 	 */
 	fun startCombatMode(combatScript: List<String>): Boolean {
-		TODO("not yet implemented")
+		try {
+			game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+			game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+			game.printToLog("[COMBAT] Starting Combat Mode.", MESSAGE_TAG = TAG)
+			game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+			game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+			
+			val commandList = combatScript.toMutableList()
+			
+			var i = 0 // Current index for the current command from the combat script.
+			var turnNumber = 1 // Current Turn number for script execution.
+			
+			// Reset the Retreat, Semi Auto, and Full Auto flags.
+			retreatCheckFlag = false
+			var semiAutoCheckFlag = false
+			var fullAutoCheckFlag = false
+			
+			if(attackButtonLocation == null) {
+				attackButtonLocation = game.imageUtils.findButton("attack", tries = 10)
+				
+				if(attackButtonLocation == null) {
+					throw(Exception("Cannot find the location of the \"Attack\" button. Shutting down Combat Mode."))
+				}
+			}
+			
+			// The following is the primary loop workflow for Combat Mode.
+			while(combatScript.isNotEmpty() && !retreatCheckFlag && !semiAutoCheckFlag && !fullAutoCheckFlag) {
+				var command = commandList.removeAt(0).toLowerCase(Locale.ROOT)
+				
+				game.printToLog("[COMBAT] Reading command: $command", MESSAGE_TAG = TAG)
+				
+				
+				if(command.contains("turn")) {
+					// Parse the Turn's number.
+					val commandTurnNumber: Int = (command.split(":")[0].split(" ")[1]).toInt()
+					
+					// If the command is a "Turn #:" and it is currently not the correct Turn, attack until the Turn numbers match.
+					if(!retreatCheckFlag && turnNumber != commandTurnNumber) {
+						game.printToLog("[COMBAT] Attacking until the bot reaches Turn $commandTurnNumber", MESSAGE_TAG = TAG)
+						
+						while(turnNumber != commandTurnNumber) {
+							game.printToLog("[COMBAT] Starting Turn $turnNumber.", MESSAGE_TAG = TAG)
+							
+							findCombatDialog()
+							
+							val tempAttackButtonLocation = game.imageUtils.findButton("attack", tries = 1, suppressError = true)
+							if(tempAttackButtonLocation != null) {
+								game.printToLog("[COMBAT] Ending Turn $turnNumber")
+								
+								val chargeAttacks = findChargeAttacks()
+								game.gestureUtils.tap(attackButtonLocation!!.x, attackButtonLocation!!.y)
+								
+								game.wait(3.0 + chargeAttacks)
+								
+								waitForAttack()
+								
+								game.printToLog("[COMBAT] Turn $turnNumber has eneded.", MESSAGE_TAG = TAG)
+								
+								partyWipeCheck()
+								turnNumber += 1
+							}
+							
+							val tempNextButtonLocation = game.imageUtils.findButton("next", tries = 1, suppressError = true)
+							if(tempNextButtonLocation != null) {
+								game.gestureUtils.tap(tempNextButtonLocation.x, tempNextButtonLocation.y)
+								game.wait(3.0)
+							}
+							
+							if(retreatCheckFlag || game.imageUtils.confirmLocation("exp_gained", tries = 1, suppressError = true) || game
+									.imageUtils.confirmLocation("no_loot", tries = 1, suppressError = true)) {
+								game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+								game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+								game.printToLog("[COMBAT] Ending Combat Mode.", MESSAGE_TAG = TAG)
+								game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+								game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+								
+								return false
+							} else if(game.imageUtils.confirmLocation("battle_concluded", tries = 1, suppressError = true)) {
+								game.printToLog("[COMBAT] Battle concluded suddenly.", MESSAGE_TAG = TAG)
+								
+								game.findAndClickButton("ok")
+								
+								game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+								game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+								game.printToLog("[COMBAT] Ending Combat Mode.", MESSAGE_TAG = TAG)
+								game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+								game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+								
+								return false
+							}
+						}
+					}
+					
+					if(!retreatCheckFlag && turnNumber == commandTurnNumber) {
+						game.printToLog("[COMBAT] Starting Turn $turnNumber.", MESSAGE_TAG = TAG)
+						
+						findCombatDialog()
+						
+						// Proceed to process each command inside this Turn block until the "end" command is reached.
+						while(!command.contains("end") && !command.contains("exit") && commandList.isNotEmpty()) {
+							command = commandList.removeAt(0)
+							
+							game.printToLog("[COMBAT] Reading command: $command", MESSAGE_TAG = TAG)
+							
+							if(command.contains("end")) {
+								break
+							} else if(command.contains("exit")) {
+								// End Combat Mode by heading back to the Home screen without retreating.
+								game.printToLog("[COMBAT] Leaving this Raid without retreating.", MESSAGE_TAG = TAG)
+								
+								game.goBackHome(confirmLocationCheck = true)
+								return false
+							}
+							
+							// Determine which Character to take action.
+							val characterSelected = when {
+								command.contains("character1") -> {
+									1
+								}
+								command.contains("character2") -> {
+									2
+								}
+								command.contains("character3") -> {
+									3
+								}
+								command.contains("character4") -> {
+									4
+								}
+								else -> {
+									0
+								}
+							}
+							
+							// Execute Skill commands here.
+							if(characterSelected != 0) {
+								// Select the specified Character.
+								selectCharacter(characterSelected)
+								
+								// Now execute each Skill command starting from left to right for this Character.
+								val skillCommandList: List<String> = command.split(".")
+								skillCommandList.drop(1)
+								
+								useCharacterSkill(characterSelected, skillCommandList)
+								
+								game.wait(3.0)
+								
+								if(game.imageUtils.confirmLocation("battle_concluded", tries = 1, suppressError = true)) {
+									game.printToLog("[COMBAT] Battle concluded suddenly.", MESSAGE_TAG = TAG)
+									
+									game.findAndClickButton("ok")
+									
+									game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+									game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+									game.printToLog("[COMBAT] Ending Combat Mode.", MESSAGE_TAG = TAG)
+									game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+									game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+									
+									return false
+								}
+							}
+							
+							// Execute Summon commands here.
+							if(command.contains("summon")) {
+								useSummon(command)
+								
+								if(game.imageUtils.confirmLocation("battle_concluded", tries = 1, suppressError = true)) {
+									game.printToLog("[COMBAT] Battle concluded suddenly.", MESSAGE_TAG = TAG)
+									
+									game.findAndClickButton("ok")
+									
+									game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+									game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+									game.printToLog("[COMBAT] Ending Combat Mode.", MESSAGE_TAG = TAG)
+									game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+									game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+									
+									return false
+								}
+							}
+							
+							if(command.contains("requestbackup")) {
+								requestBackup()
+							} else if(command.contains("tweetbackup")) {
+								tweetBackup()
+							} else if(healingItemCommands.contains(command)) {
+								useCombatHealingItem(command)
+							} else if(command.contains("enablesemiauto")) {
+								game.printToLog("[COMBAT] Enabling Semi Auto.", MESSAGE_TAG = TAG)
+								semiAutoCheckFlag = true
+								break
+							} else if(command.contains("enablefullauto")) {
+								game.printToLog("[COMBAT] Enabling Full Auto.", MESSAGE_TAG = TAG)
+								val enabledCheckFlag = game.findAndClickButton("full_auto")
+								
+								// If the bot failed to find and click the "Full Auto" button, fallback to the "Semi Auto" button.
+								if(!enabledCheckFlag) {
+									game.printToLog("[COMBAT] Failed to find the \"Full Auto\" button. Falling back to Semi Auto.", MESSAGE_TAG = TAG)
+									semiAutoCheckFlag = true
+								} else {
+									fullAutoCheckFlag = true
+								}
+								
+								break
+							}
+							
+							val nextButtonLocation = game.imageUtils.findButton("next", tries = 1, suppressError = true)
+							if(nextButtonLocation != null) {
+								game.gestureUtils.tap(nextButtonLocation.x, nextButtonLocation.y)
+								break
+							}
+						}
+					}
+					
+					if(!semiAutoCheckFlag && !fullAutoCheckFlag && command.contains("enablesemiauto")) {
+						game.printToLog("[COMBAT] Enabling Semi Auto.", MESSAGE_TAG = TAG)
+						semiAutoCheckFlag = true
+						break
+					} else if(!semiAutoCheckFlag && !fullAutoCheckFlag && command.contains("enablefullauto")) {
+						game.printToLog("[COMBAT] Enabling Full Auto.", MESSAGE_TAG = TAG)
+						val enabledCheckFlag = game.findAndClickButton("full_auto")
+						
+						// If the bot failed to find and click the "Full Auto" button, fallback to the "Semi Auto" button.
+						if(!enabledCheckFlag) {
+							game.printToLog("[COMBAT] Failed to find the \"Full Auto\" button. Falling back to Semi Auto.", MESSAGE_TAG = TAG)
+							semiAutoCheckFlag = true
+						} else {
+							fullAutoCheckFlag = true
+						}
+						
+						break
+					} else if(!semiAutoCheckFlag && !fullAutoCheckFlag && command == "end") {
+						// End the current Turn block and attack.
+						var nextButtonLocation = game.imageUtils.findButton("next", tries = 1, suppressError = true)
+						
+						if(nextButtonLocation != null) {
+							game.printToLog("[COMBAT] All enemies on screen have been eliminated before the bot could attack. Preserving Turn " +
+									"$turnNumber by moving on to the next Wave.", MESSAGE_TAG = TAG)
+							
+							game.gestureUtils.tap(nextButtonLocation.x, nextButtonLocation.y)
+							
+							game.wait(3.0)
+						} else {
+							game.printToLog("[COMBAT] Ending Turn $turnNumber by attacking now.", MESSAGE_TAG = TAG)
+							
+							val chargeAttacks = findChargeAttacks()
+							game.gestureUtils.tap(attackButtonLocation!!.x, attackButtonLocation!!.y)
+							
+							// Peek ahead of the combat script while the Party is currently attacking and see if it detects the command
+							// "enableSemiAuto" outside of a Turn block.
+							var tempIndex = 0
+							if(combatScript.isNotEmpty()) {
+								val tempCommand = combatScript[tempIndex]
+								
+								if(!semiAutoCheckFlag && !fullAutoCheckFlag && tempCommand.contains("enablesemiauto")) {
+									game.printToLog("[COMBAT] Enabling Semi Auto.", MESSAGE_TAG = TAG)
+									game.findAndClickButton("semi_auto")
+									semiAutoCheckFlag = true
+									break
+								} else if(tempCommand.contains("turn")) {
+									break
+								}
+								
+								tempIndex += 1
+							}
+							
+							game.wait(3.0 + chargeAttacks)
+							
+							waitForAttack()
+							
+							game.printToLog("[COMBAT] Turn $turnNumber has ended.", MESSAGE_TAG = TAG)
+							
+							partyWipeCheck()
+							turnNumber += 1
+							
+							nextButtonLocation = game.imageUtils.findButton("next", tries = 1, suppressError = true)
+							if(nextButtonLocation != null) {
+								game.printToLog("[COMBAT] All enemies on screen have been eliminated before the bot could attack. Preserving Turn " +
+										"$turnNumber by moving on to the next Wave.", MESSAGE_TAG = TAG)
+								
+								game.gestureUtils.tap(nextButtonLocation.x, nextButtonLocation.y)
+								
+								game.wait(3.0)
+							}
+						}
+					} else if(!semiAutoCheckFlag && !fullAutoCheckFlag && command == "exit") {
+						game.printToLog("[COMBAT] Leaving this Raid without retreating.", MESSAGE_TAG = TAG)
+						
+						game.goBackHome(confirmLocationCheck = true)
+						return false
+					}
+				}
+			}
+			
+			// When the bot arrives here, that means it has either processed all the commands inside the combat script, the Party retreated, or
+			// Full Auto or Semi Auto was turned on.
+			game.printToLog("[COMBAT] Bot has processed the entire combat script. Automatically attacking until the battle ends or Party wipes.",
+				MESSAGE_TAG = TAG)
+			
+			// Double check to see if Semi Auto is turned on. Note that the "Semi Auto" button only appears while the Party is attacking.
+			if(!retreatCheckFlag && semiAutoCheckFlag) {
+				game.printToLog("[COMBAT] Double checking to see if Semi Auto is enabled.", MESSAGE_TAG = TAG)
+				
+				var enabledSemiAutoButtonLocation = game.imageUtils.findButton("semi_auto_enabled")
+				if(enabledSemiAutoButtonLocation == null) {
+					// Have the Party attack and then attempt to see if the "Semi Auto" button becomes visible.
+					game.findAndClickButton("attack")
+					
+					game.wait(2.0)
+					
+					enabledSemiAutoButtonLocation = game.imageUtils.findButton("semi_auto")
+					
+					// If the bot still cannot find the "Semi Auto" button, that probably means that the user has the "Full Auto" button on the
+					// screen instead.
+					if(enabledSemiAutoButtonLocation == null) {
+						game.printToLog("[COMBAT] Failed to enable Semi Auto. Falling back to Full Auto.", MESSAGE_TAG = TAG)
+						semiAutoCheckFlag = false
+						fullAutoCheckFlag = true
+						
+						// Enable Full Auto.
+						game.findAndClickButton("full_auto")
+					} else {
+						game.printToLog("[COMBAT] Semi Auto is now enabled.", MESSAGE_TAG = TAG)
+					}
+				} else {
+					game.gestureUtils.tap(enabledSemiAutoButtonLocation.x, enabledSemiAutoButtonLocation.y)
+					game.printToLog("[COMBAT] Semi Auto is now enabled.", MESSAGE_TAG = TAG)
+				}
+			}
+			
+			// Primary loop workflow for Semi Auto / Full Auto.
+			while(!retreatCheckFlag && (semiAutoCheckFlag || fullAutoCheckFlag) && !game.imageUtils.confirmLocation("exp_gained", tries = 1,
+					suppressError = true) && !game.imageUtils.confirmLocation("no_loot", tries = 1, suppressError = true)) {
+				if(game.imageUtils.confirmLocation("battle_concluded", tries = 1, suppressError = true)) {
+					game.printToLog("[COMBAT] Battle concluded suddenly.", MESSAGE_TAG = TAG)
+					game.findAndClickButton("ok")
+					break
+				}
+				
+				partyWipeCheck()
+				
+				game.wait(3.0)
+			}
+			
+			// Until the bot reaches the Quest Results screen, keep pressing the "Attack" and "Next" buttons if not Semi Auto or Full Auto.
+			while(!retreatCheckFlag && !semiAutoCheckFlag && !fullAutoCheckFlag && !game.imageUtils.confirmLocation("exp_gained", tries = 1,
+					suppressError = true) && !game.imageUtils.confirmLocation("no_loot", tries = 1, suppressError = true)) {
+				findCombatDialog()
+				
+				val tempAttackButtonLocation = game.imageUtils.findButton("attack", tries = 1, suppressError = true)
+				if(tempAttackButtonLocation != null) {
+					game.printToLog("[COMBAT] Starting Turn $turnNumber.", MESSAGE_TAG = TAG)
+					game.printToLog("[COMBAT] Ending Turn $turnNumber by attacking now.", MESSAGE_TAG = TAG)
+					
+					val chargeAttacks = findChargeAttacks()
+					game.gestureUtils.tap(attackButtonLocation!!.x, attackButtonLocation!!.y)
+					
+					game.wait(3.0 + chargeAttacks)
+					
+					waitForAttack()
+					
+					game.printToLog("[COMBAT] Turn $turnNumber has ended.", MESSAGE_TAG = TAG)
+					
+					partyWipeCheck()
+					turnNumber += 1
+				}
+				
+				val nextButtonLocation = game.imageUtils.findButton("next", tries = 1, suppressError = true)
+				if(nextButtonLocation != null) {
+					game.gestureUtils.tap(nextButtonLocation.x, nextButtonLocation.y)
+					game.wait(3.0)
+				}
+			}
+			
+			game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+			game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+			game.printToLog("[COMBAT] Ending Combat Mode.", MESSAGE_TAG = TAG)
+			game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+			game.printToLog("################################################################################", MESSAGE_TAG = TAG)
+			
+			return if(!retreatCheckFlag) {
+				game.printToLog("[INFO] Bot has reached the Quest Results screen.", MESSAGE_TAG = TAG)
+				true
+			} else {
+				false
+			}
+		} catch(e: Exception) {
+			game.printToLog("[ERROR] Encountered exception during Combat Mode: ${e.printStackTrace()}", MESSAGE_TAG = TAG)
+		}
+		
+		return false
 	}
 }
