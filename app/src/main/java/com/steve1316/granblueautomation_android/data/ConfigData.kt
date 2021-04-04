bfeb538eb900f02001d6a67877ab4d89aafeeb3d
Steve Tu <stevetu11@yahoo.com>
2021-04-03 19:12:46 -0700

    Created ConfigData so that Hoplite can read user settings from config.yaml

---
+++ app/src/main/java/com/steve1316/granblueautomation_android/data/ConfigData.kt
@@ -0,0 +1,87 @@
+package com.steve1316.granblueautomation_android.data
+
+/**
+ * This is structured in such a way that Hoplite can parse the config.yaml file and create the following data classes from it.
+ */
+data class ConfigData(
+	val twitter: TwitterData,
+	val refill: RefillData,
+	val delayBetweenRuns: DelayBetweenRunsData,
+	val idleAfterAction: IdleAfterActionData,
+	val dimensionalHalo: DimensionalHaloData,
+	val event: EventNightmareData,
+	val rotb: RiseOfTheBeastsData,
+	val dreadBarrage: DreadBarrageData
+)
+
+/**
+ * Consumer keys and access tokens required to use the Twitter API.
+ */
+data class TwitterData(val apiKey: String, val apiKeySecret: String, val accessToken: String, val accessTokenSecret: String)
+
+/**
+ * Determine whether or not the user wants to use Full Elixir/Soul Balm to refill.
+ */
+data class RefillData(val fullElixir: Boolean = false, val soulBalm: Boolean = false)
+
+/**
+ * Enable delays after every successful run.
+ */
+data class DelayBetweenRunsData(
+	val enableDelayBetweenRuns: Boolean = false,
+	val delayInSeconds: Int = 15,
+	val enableRandomizedDelayBetweenRuns: Boolean = false,
+	val delayInSecondsLowerBound: Int = 15,
+	val delayInSecondsUpperBound: Int = 60
+)
+
+/**
+ * Enable delays after every Skill or Summon action.
+ */
+data class IdleAfterActionData(val idleAfterSkillInSeconds: Int = 4, val idleAfterSummonInSeconds: Int = 7)
+
+/**
+ * Settings needed to do Dimensional Halo.
+ */
+data class DimensionalHaloData(
+	val enableDimensionalHalo: Boolean = false,
+	val dimensionalHaloCombatScript: String = "",
+	val dimensionalHaloSummonList: List<String> = listOf(),
+	val dimensionalHaloGroupNumber: Int = 0,
+	val dimensionalHaloPartyNumber: Int = 0
+)
+
+/**
+ * Settings needed to do Event Nightmare.
+ */
+data class EventNightmareData(
+	val enableEventNightmare: Boolean = false,
+	val eventNightmareCombatScript: String = "",
+	val eventNightmareSummonList: List<String> = listOf(),
+	val eventNightmareGroupNumber: Int = 0,
+	val eventNightmarePartyNumber: Int = 0
+)
+
+/**
+ * Settings needed to do Rise of the Beasts Extreme+.
+ */
+data class RiseOfTheBeastsData(
+	val enableROTBExtremePlus: Boolean = false,
+	val rotbExtremePlusCombatScript: String = "",
+	val rotbExtremePlusSummonList: List<String> = listOf(),
+	val rotbExtremePlusGroupNumber: Int = 0,
+	val rotbExtremePlusPartyNumber: Int = 0
+)
+
+/**
+ * Settings needed to do Dread Barrage Unparalleled Foes.
+ */
+data class DreadBarrageData(
+	val enableUnparalleledFoe: Boolean = false,
+	val enableUnparalleledFoeLevel95: Boolean = false,
+	val enableUnparalleledFoeLevel175: Boolean = false,
+	val unparalleledFoeCombatScript: String = "",
+	val unparalleledFoeSummonList: List<String> = listOf(),
+	val unparalleledFoeGroupNumber: Int = 0,
+	val unparalleledFoePartyNumber: Int = 0
+)
