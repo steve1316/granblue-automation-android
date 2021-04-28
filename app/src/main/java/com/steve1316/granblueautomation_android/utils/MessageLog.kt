48624ff30a5994c2911dc4e513344a27998a5fbc
Steve Tu <stevetu11@yahoo.com>
2021-04-28 00:33:49 -0700

    Added a flag to check if the log was saved already to prevent double saving.

--- app/src/main/java/com/steve1316/granblueautomation_android/utils/MessageLog.kt
+++ app/src/main/java/com/steve1316/granblueautomation_android/utils/MessageLog.kt
@@ -14,35 +14,41 @@ class MessageLog {
 		private const val TAG: String = "GAA_MessageLog"
 		var messageLog = arrayListOf<String>()
 		
+		var saveCheck = false
+		
 		/**
 		 * Save the current Message Log into a new file inside internal storage's /logs/ folder.
 		 *
 		 * @param context The context for the application.
 		 */
 		fun saveLogToFile(context: Context) {
-			Log.d(TAG, "Now beginning process to save current Message Log to internal storage...")
-			
-			// Generate file path to save to. All message logs will be saved to the /logs/ folder inside internal storage. Create the /logs/ folder if needed.
-			val path = File(context.getExternalFilesDir(null)?.absolutePath + "/logs/")
-			if (!path.exists()) {
-				path.mkdirs()
-			}
-			
-			// Generate the file name.
-			val current = LocalDateTime.now()
-			val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-			val fileName = "log @ ${current.format(formatter)}"
-			
-			// Now save the Message Log to the new text file.
-			Log.d(TAG, "Now saving Message Log to file named \"$fileName\" at $path")
-			val file = File(path, "$fileName.txt")
-			
-			if (!file.exists()) {
-				file.createNewFile()
-				file.printWriter().use { out ->
-					messageLog.forEach {
-						out.println(it)
+			if (!saveCheck) {
+				Log.d(TAG, "Now beginning process to save current Message Log to internal storage...")
+				
+				// Generate file path to save to. All message logs will be saved to the /logs/ folder inside internal storage. Create the /logs/ folder if needed.
+				val path = File(context.getExternalFilesDir(null)?.absolutePath + "/logs/")
+				if (!path.exists()) {
+					path.mkdirs()
+				}
+				
+				// Generate the file name.
+				val current = LocalDateTime.now()
+				val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+				val fileName = "log @ ${current.format(formatter)}"
+				
+				// Now save the Message Log to the new text file.
+				Log.d(TAG, "Now saving Message Log to file named \"$fileName\" at $path")
+				val file = File(path, "$fileName.txt")
+				
+				if (!file.exists()) {
+					file.createNewFile()
+					file.printWriter().use { out ->
+						messageLog.forEach {
+							out.println(it)
+						}
 					}
+					
+					saveCheck = true
 				}
 			}
 		}
