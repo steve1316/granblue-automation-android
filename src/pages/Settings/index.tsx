580fffc738c0b03b1b7ae5987dd66e534dc11683
Steve Tu <stevetu11@yahoo.com>
2021-12-25 18:10:17 -0800

    Add dropdown pickers for Farming Mode, Item, and Mission

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,18 +1,271 @@
-import { StyleSheet, View, Text } from "react-native"
+import { useContext, useEffect, useState } from "react"
+import { StyleSheet, View, ScrollView } from "react-native"
+import DropDownPicker from "react-native-dropdown-picker"
+import { Divider } from "react-native-elements"
+import { Button } from "react-native-elements/dist/buttons/Button"
+import DocumentPicker, { DirectoryPickerResponse, DocumentPickerResponse } from "react-native-document-picker"
+import data from "../../data/data.json"
+import { BotStateContext } from "../../context/BotStateContext"
 
 const styles = StyleSheet.create({
-    container: {
+    root: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
         justifyContent: "center",
+    },
+    picker: {
+        marginVertical: 20,
+    },
+    dropdown: {
+        marginTop: 20,
     },
 })
 
+interface Item {
+    label: string
+    value: string
+}
+
 const Settings = () => {
+    const [open, setOpen] = useState<boolean>(false)
+    const [open2, setOpen2] = useState<boolean>(false)
+    const [open3, setOpen3] = useState<boolean>(false)
+
+    const [itemList, setItemList] = useState<Item[]>([])
+    const [missionList, setMissionList] = useState<Item[]>([])
+
+    const [farmingMode, setFarmingMode] = useState<string>("")
+    const [item, setItem] = useState<string>("")
+    const [mission, setMission] = useState<string>("")
+    const [itemAmount, setItemAmount] = useState<number>(1)
+
+    const [result, setResult] = useState<Array<DocumentPickerResponse> | DirectoryPickerResponse | undefined | null>()
+
+    const bsc = useContext(BotStateContext)
+
+    const farmingModes = [
+        { label: "Quest", value: "Quest" },
+        { label: "Special", value: "Special" },
+        { label: "Coop", value: "Coop" },
+        { label: "Raid", value: "Raid" },
+        { label: "Event", value: "Event" },
+        { label: "Event (Token Drawboxes)", value: "Event (Token Drawboxes)" },
+        { label: "Rise of the Beasts", value: "Rise of the Beasts" },
+        { label: "Guild Wars", value: "Guild Wars" },
+        { label: "Dread Barrage", value: "Dread Barrage" },
+        { label: "Proving Grounds", value: "Proving Grounds" },
+        { label: "Xeno Clash", value: "Xeno Clash" },
+        { label: "Arcarum", value: "Arcarum" },
+        { label: "Generic", value: "Generic" },
+    ]
+
+    const closeAllPickers = () => {
+        setOpen(false)
+        setOpen2(false)
+    }
+
+    // useEffect(() => {
+    //     console.log(JSON.stringify(result, null, 2))
+    // }, [result])
+
+    //////////////////////////////////////////////////
+    // Callbacks to save current settings.
+    useEffect(() => {
+        // Reset selected Item and Mission.
+        bsc.setSettings({
+            ...bsc.settings,
+            game: { ...bsc.settings.game, farmingMode: farmingMode, item: "", mission: "", map: "" },
+            nightmare: {
+                ...bsc.settings.nightmare,
+                enableNightmare: false,
+                enableCustomNightmareSettings: false,
+                nightmareCombatScriptName: "",
+                nightmareCombatScript: [],
+                nightmareSummons: [],
+                nightmareSummonElements: [],
+                nightmareGroupNumber: 1,
+                nightmarePartyNumber: 1,
+            },
+        })
+
+        setItem("")
+        setMission("")
+    }, [farmingMode])
+
+    useEffect(() => {
+        bsc.setSettings({
+            ...bsc.settings,
+            game: { ...bsc.settings.game, farmingMode: farmingMode, item: item, mission: mission, map: "" },
+        })
+    }, [item, mission])
+
+    useEffect(() => {
+        console.log("Item populate callback is called")
+        populateItemList()
+    }, [farmingMode])
+
+    useEffect(() => {
+        console.log("Mission populate callback is called")
+        populateMissionList()
+    }, [item])
+
+    const populateItemList = () => {
+        let newItemList: Item[] = []
+
+        if (
+            farmingMode === "Quest" ||
+            farmingMode === "Special" ||
+            farmingMode === "Coop" ||
+            farmingMode === "Raid" ||
+            farmingMode === "Event" ||
+            farmingMode === "Event (Token Drawboxes)" ||
+            farmingMode === "Rise of the Beasts" ||
+            farmingMode === "Guild Wars" ||
+            farmingMode === "Dread Barrage" ||
+            farmingMode === "Proving Grounds" ||
+            farmingMode === "Xeno Clash" ||
+            farmingMode === "Arcarum" ||
+            farmingMode === "Generic"
+        ) {
+            Object.values(data[farmingMode]).forEach((tempItems) => {
+                tempItems.items.forEach((item) => {
+                    newItemList = newItemList.concat({ label: item, value: item })
+                })
+            })
+        }
+
+        const filteredNewItemList = newItemList.filter((v, i, a) => a.findIndex((t) => t.label === v.label) === i)
+        setItemList(filteredNewItemList)
+    }
+
+    const populateMissionList = () => {
+        let newMissionList: Item[] = []
+
+        if (
+            farmingMode === "Quest" ||
+            farmingMode === "Special" ||
+            farmingMode === "Coop" ||
+            farmingMode === "Raid" ||
+            farmingMode === "Event" ||
+            farmingMode === "Event (Token Drawboxes)" ||
+            farmingMode === "Rise of the Beasts" ||
+            farmingMode === "Guild Wars" ||
+            farmingMode === "Dread Barrage" ||
+            farmingMode === "Proving Grounds" ||
+            farmingMode === "Xeno Clash" ||
+            farmingMode === "Arcarum" ||
+            farmingMode === "Generic"
+        ) {
+            Object.entries(data[farmingMode]).forEach((obj) => {
+                if (obj[1].items.indexOf(item) !== -1) {
+                    newMissionList = newMissionList.concat({ label: obj[0], value: obj[0] })
+                }
+            })
+        } else {
+            Object.entries(data["Coop"]).forEach((obj) => {
+                if (obj[1].items.indexOf(item) !== -1) {
+                    newMissionList = newMissionList.concat({ label: obj[0], value: obj[0] })
+                }
+            })
+        }
+
+        const filteredNewMissionList = Array.from(new Set(newMissionList))
+        setMissionList(filteredNewMissionList)
+    }
+
+    useEffect(() => {
+        console.log("AMOUNT: ", itemAmount)
+    }, [itemAmount])
+
     return (
-        <View style={styles.container}>
-            <Text>Hello Settings</Text>
+        <View style={styles.root}>
+            <ScrollView nestedScrollEnabled={true}>
+                <View style={{ height: 1000, marginHorizontal: 20 }}>
+                    {/* <Button
+                        title="Select Combat Script"
+                        buttonStyle={{
+                            backgroundColor: "rgba(78, 116, 289, 1)",
+                            borderRadius: 3,
+                        }}
+                        containerStyle={{
+                            width: 100,
+                            marginHorizontal: 50,
+                            marginVertical: 10,
+                        }}
+                        raised
+                        onPress={async () => {
+                            try {
+                                const pickerResult = await DocumentPicker.pickSingle({
+                                    presentationStyle: "fullScreen",
+                                })
+                                setResult([pickerResult])
+                            } catch (e) {
+                                console.error(e)
+                            }
+                        }}
+                    /> */}
+
+                    <Divider />
+
+                    <DropDownPicker
+                        listMode="SCROLLVIEW"
+                        style={styles.picker}
+                        dropDownContainerStyle={styles.dropdown}
+                        placeholder="Select Farming Mode"
+                        searchTextInputStyle={{ fontStyle: "italic" }}
+                        items={farmingModes}
+                        open={open}
+                        setOpen={(flag) => {
+                            closeAllPickers()
+                            setOpen(flag)
+                        }}
+                        value={farmingMode}
+                        setValue={setFarmingMode}
+                        zIndex={9999}
+                    />
+
+                    {farmingMode !== "" ? (
+                        <DropDownPicker
+                            listMode="MODAL"
+                            modalProps={{
+                                animationType: "slide",
+                            }}
+                            style={styles.picker}
+                            dropDownContainerStyle={styles.dropdown}
+                            placeholder="Select Item"
+                            searchTextInputStyle={{ fontStyle: "italic" }}
+                            searchable={true}
+                            items={itemList}
+                            open={open2}
+                            setOpen={(flag) => {
+                                closeAllPickers()
+                                setOpen2(flag)
+                            }}
+                            value={item}
+                            setValue={setItem}
+                            zIndex={9998}
+                        />
+                    ) : null}
+
+                    {item !== "" ? (
+                        <DropDownPicker
+                            listMode="SCROLLVIEW"
+                            style={styles.picker}
+                            dropDownContainerStyle={styles.dropdown}
+                            placeholder="Select Mission"
+                            searchTextInputStyle={{ fontStyle: "italic" }}
+                            items={missionList}
+                            open={open3}
+                            setOpen={(flag) => {
+                                closeAllPickers()
+                                setOpen3(flag)
+                            }}
+                            value={mission}
+                            setValue={setMission}
+                            zIndex={9997}
+                        />
+                    ) : null}
+                </View>
+            </ScrollView>
         </View>
     )
 }
