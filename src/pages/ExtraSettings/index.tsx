89cfa7c97969b244116c1caa2fca8e4ecf934dc7
Steve Tu <stevetu11@yahoo.com>
2021-12-26 22:13:46 -0800

    Create Checkbox component and add conditional rendering for some

--- src/pages/ExtraSettings/index.tsx
+++ src/pages/ExtraSettings/index.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { StyleSheet, View, ScrollView } from "react-native"
-import BouncyCheckbox from "react-native-bouncy-checkbox"
 import { Input, Text } from "react-native-elements"
+import Checkbox from "../../components/Checkbox"
+import BouncyCheckbox from "react-native-bouncy-checkbox"
 import TitleDivider from "../../components/TitleDivider"
 
 const styles = StyleSheet.create({
@@ -21,26 +22,12 @@ const ExtraSettings = () => {
     const [twitterAccessTokenSecret, setTwitterAccessTokenSecret] = useState<string>("")
     const [discordToken, setDiscordToken] = useState<string>("")
     const [discordUserID, setDiscordUserID] = useState<string>("")
+    const [enableDelayBetweenRuns, setEnableDelayBetweenRuns] = useState<boolean>(false)
+    const [enableRandomizedDelayBetweenRuns, setEnableRandomizedDelayBetweenRuns] = useState<boolean>(false)
 
     return (
         <View style={styles.root}>
             <ScrollView>
-                <BouncyCheckbox
-                    size={30}
-                    fillColor="red"
-                    unfillColor="white"
-                    text={"Enable Debug Mode"}
-                    iconStyle={{ borderColor: "red" }}
-                    textStyle={{
-                        textDecorationLine: "none",
-                    }}
-                    style={{ marginVertical: 10 }}
-                    isChecked={debugMode}
-                    onPress={(isChecked: boolean) => {
-                        setDebugMode(isChecked)
-                    }}
-                />
-
                 <TitleDivider
                     title="Twitter Settings"
                     subtitle="Please visit the wiki on the GitHub page for instructions on how to get these keys and tokens."
@@ -55,13 +42,66 @@ const ExtraSettings = () => {
 
                 <TitleDivider
                     title="Discord Settings"
-                    subtitle="Please visit the wiki on the GitHub page for instructions on how to get the token and user ID."
+                    subtitle={`Please visit the wiki on the GitHub page for instructions on how to get the token and user ID.\n\nNote: This does not work on emulators.`}
                     hasIcon={true}
                     iconName="discord"
                     iconColor="#7289d9"
                 />
-                <Input label="Discord Token" multiline value={discordToken} onChangeText={(value: string) => setDiscordToken(value)} />
-                <Input label="Discord User ID" multiline value={discordUserID} onChangeText={(value: string) => setDiscordUserID(value)} />
+                <Input label="Discord Token" multiline containerStyle={{ marginLeft: -10 }} value={discordToken} onChangeText={(value: string) => setDiscordToken(value)} />
+                <Input label="Discord User ID" multiline containerStyle={{ marginLeft: -10 }} value={discordUserID} onChangeText={(value: string) => setDiscordUserID(value)} />
+
+                <TitleDivider title="Configuration Settings" hasIcon={true} iconName="tune" />
+                <Checkbox text="Enable Debug Mode" subtitle="Enables debugging messages to show up in the log." state={debugMode} updateState={setDebugMode} />
+
+                {!enableRandomizedDelayBetweenRuns ? (
+                    <View>
+                        <BouncyCheckbox
+                            size={30}
+                            fillColor={"red"}
+                            unfillColor={"white"}
+                            text="Enable Delay Between Runs"
+                            iconStyle={{ borderColor: "red" }}
+                            textStyle={{
+                                textDecorationLine: "none",
+                            }}
+                            style={{ marginVertical: 10, marginLeft: 2 }}
+                            isChecked={enableDelayBetweenRuns}
+                            onPress={(checked) => {
+                                if (checked && enableRandomizedDelayBetweenRuns) {
+                                    setEnableRandomizedDelayBetweenRuns(false)
+                                }
+
+                                setEnableDelayBetweenRuns(checked)
+                            }}
+                        />
+                        <Text style={{ marginBottom: 5 }}>Enable delay in seconds between runs to serve as a resting period.</Text>
+                    </View>
+                ) : null}
+
+                {!enableDelayBetweenRuns ? (
+                    <View>
+                        <BouncyCheckbox
+                            size={30}
+                            fillColor={"red"}
+                            unfillColor={"white"}
+                            text="Enable Randomized Delay Between Runs"
+                            iconStyle={{ borderColor: "red" }}
+                            textStyle={{
+                                textDecorationLine: "none",
+                            }}
+                            style={{ marginVertical: 10, marginLeft: 2 }}
+                            isChecked={enableRandomizedDelayBetweenRuns}
+                            onPress={(checked) => {
+                                if (checked && enableDelayBetweenRuns) {
+                                    setEnableDelayBetweenRuns(false)
+                                }
+
+                                setEnableRandomizedDelayBetweenRuns(checked)
+                            }}
+                        />
+                        <Text style={{ marginBottom: 5 }}>Enable randomized delay in seconds between runs to serve as a resting period.</Text>
+                    </View>
+                ) : null}
             </ScrollView>
         </View>
     )
