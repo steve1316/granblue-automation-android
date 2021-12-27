6a44539ab88b5ba45532730b86594998e1c57f46
Steve Tu <stevetu11@yahoo.com>
2021-12-26 20:14:11 -0800

    Add checkbox component to toggle Debug Mode

--- src/pages/ExtraSettings/index.tsx
+++ src/pages/ExtraSettings/index.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, View, Text } from "react-native"
+import BouncyCheckbox from "react-native-bouncy-checkbox"
 
 const styles = StyleSheet.create({
     root: {
@@ -13,6 +14,21 @@ const ExtraSettings = () => {
     return (
         <View style={styles.root}>
             <Text>Hello Extra Settings</Text>
+                <BouncyCheckbox
+                    size={30}
+                    fillColor="red"
+                    unfillColor="white"
+                    text={"Enable Debug Mode"}
+                    iconStyle={{ borderColor: "red" }}
+                    textStyle={{
+                        textDecorationLine: "none",
+                    }}
+                    style={{ marginVertical: 10 }}
+                    isChecked={debugMode}
+                    onPress={(isChecked: boolean) => {
+                        setDebugMode(isChecked)
+                    }}
+                />
         </View>
     )
 }
