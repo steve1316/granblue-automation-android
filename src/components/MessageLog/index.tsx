9a60869a8707072d1c4fc21a3a2b30f776adb533
Steve Tu <stevetu11@yahoo.com>
2022-01-09 22:25:55 -0800

    Create MessageLogContext to hold message log state

--- src/components/MessageLog/index.tsx
+++ src/components/MessageLog/index.tsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useContext } from "react"
 import { StyleSheet, View, ScrollView, Text } from "react-native"
+import { MessageLogContext } from "../../context/MessageLogContext"
 
 const styles = StyleSheet.create({
     logInnerContainer: {
@@ -18,19 +19,18 @@ const styles = StyleSheet.create({
     },
 })
 
-const introMessage = `
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sit amet consectetur adipiscing. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Suscipit adipiscing bibendum est ultricies integer quis auctor. Dolor sit amet consectetur adipiscing elit. Enim nec dui nunc mattis enim ut tellus elementum. Ultrices eros in cursus turpis. Arcu cursus euismod quis viverra nibh cras pulvinar mattis. Tellus orci ac auctor augue mauris augue neque. Est ante in nibh mauris cursus mattis. Venenatis cras sed felis eget velit aliquet sagittis id. Egestas pretium aenean pharetra magna ac. Ornare lectus sit amet est placerat in egestas. Vitae ultricies leo integer malesuada nunc vel risus. Nunc faucibus a pellentesque sit amet porttitor. At in tellus integer feugiat scelerisque varius morbi. Facilisis sed odio morbi quis. Elementum sagittis vitae et leo.
-
-Libero nunc consequat interdum varius sit amet mattis vulputate enim. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Mollis nunc sed id semper risus in hendrerit gravida. Quisque id diam vel quam elementum. Risus feugiat in ante metus. Sit amet luctus venenatis lectus magna fringilla urna. Bibendum neque egestas congue quisque egestas diam in. Porttitor massa id neque aliquam. Eget gravida cum sociis natoque penatibus et magnis dis. Viverra adipiscing at in tellus. Ante in nibh mauris cursus mattis molestie a iaculis. Nibh tortor id aliquet lectus. Vitae semper quis lectus nulla at volutpat diam ut venenatis. Mauris sit amet massa vitae tortor condimentum lacinia quis vel.
-
-Eu sem integer vitae justo eget magna. Fermentum odio eu feugiat pretium nibh. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla. Consequat id porta nibh venenatis cras sed felis eget velit. Interdum posuere lorem ipsum dolor sit amet. Purus gravida quis blandit turpis cursus in hac habitasse platea. Pharetra convallis posuere morbi leo urna molestie at elementum. Aliquam eleifend mi in nulla posuere. Risus in hendrerit gravida rutrum quisque non tellus orci. A pellentesque sit amet porttitor eget. Non tellus orci ac auctor augue mauris augue. Sem et tortor consequat id porta nibh. Fusce id velit ut tortor pretium viverra suspendisse.
-`
-
 const MessageLog = () => {
+    const mlc = useContext(MessageLogContext)
+
+    const introMessage = `****************************************\nWelcome to Granblue Automation Android!\n****************************************\nInstructions\n----------------\nNote: The START button is disabled until the following steps are followed through.\n
+    1. Have your game window's Bottom Menu visible. Set the game window size set to the second "notch". 
+    2. Go to the Settings Page of the bot and fill out the sections.
+    3. You can now head back to the Home Page of the bot and click START.\n****************************************\n`
+
     return (
         <View style={styles.logInnerContainer}>
             <ScrollView>
-                <Text style={styles.logText}>{introMessage}</Text>
+                <Text style={styles.logText}>{introMessage + mlc.messageLog.join("\r")}</Text>
             </ScrollView>
         </View>
     )
