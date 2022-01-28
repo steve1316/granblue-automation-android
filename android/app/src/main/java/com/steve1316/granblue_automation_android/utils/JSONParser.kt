68cf957c1b80ad99be86853ec11f050b537579f6
Steve Tu <stevetu11@yahoo.com>
2022-01-28 09:32:40 -0800

    Fix freezing at bot start when new fields are missing from settings.json

--- android/app/src/main/java/com/steve1316/granblue_automation_android/utils/JSONParser.kt
+++ android/app/src/main/java/com/steve1316/granblue_automation_android/utils/JSONParser.kt
@@ -10,7 +10,7 @@ import org.json.JSONArray
 import org.json.JSONObject
 import java.io.File
 
-class JSONParser() {
+class JSONParser {
 	private val tag = "${loggerTag}JSONParser"
 
 	/**
@@ -31,122 +31,167 @@ class JSONParser() {
 
 		val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(myContext)
 
-		val gameObj = jObj.getJSONObject("game")
-		sharedPreferences.edit {
-			putString("combatScriptName", gameObj.getString("combatScriptName"))
-			putString("combatScript", toArrayList(gameObj.getJSONArray("combatScript")).joinToString("|"))
-			putString("farmingMode", gameObj.getString("farmingMode"))
-			putString("item", gameObj.getString("item"))
-			putString("mission", gameObj.getString("mission"))
-			putString("map", gameObj.getString("map"))
-			putInt("itemAmount", gameObj.getInt("itemAmount"))
-			putString("summons", toArrayList(gameObj.getJSONArray("summons")).joinToString("|"))
-			putString("summonElements", toArrayList(gameObj.getJSONArray("summonElements")).joinToString("|"))
-			putInt("groupNumber", gameObj.getInt("groupNumber"))
-			putInt("partyNumber", gameObj.getInt("partyNumber"))
-			putBoolean("debugMode", gameObj.getBoolean("debugMode"))
-			commit()
+		try {
+			val gameObj = jObj.getJSONObject("game")
+			sharedPreferences.edit {
+				putString("combatScriptName", gameObj.getString("combatScriptName"))
+				putString("combatScript", toArrayList(gameObj.getJSONArray("combatScript")).joinToString("|"))
+				putString("farmingMode", gameObj.getString("farmingMode"))
+				putString("item", gameObj.getString("item"))
+				putString("mission", gameObj.getString("mission"))
+				putString("map", gameObj.getString("map"))
+				putInt("itemAmount", gameObj.getInt("itemAmount"))
+				putString("summons", toArrayList(gameObj.getJSONArray("summons")).joinToString("|"))
+				putString("summonElements", toArrayList(gameObj.getJSONArray("summonElements")).joinToString("|"))
+				putInt("groupNumber", gameObj.getInt("groupNumber"))
+				putInt("partyNumber", gameObj.getInt("partyNumber"))
+				putBoolean("debugMode", gameObj.getBoolean("debugMode"))
+				commit()
+			}
+		} catch (e: Exception) {
 		}
 
-		val twitterObj = jObj.getJSONObject("twitter")
-		sharedPreferences.edit {
-			putString("twitterAPIKey", twitterObj.getString("twitterAPIKey"))
-			putString("twitterAPIKeySecret", twitterObj.getString("twitterAPIKeySecret"))
-			putString("twitterAccessToken", twitterObj.getString("twitterAccessToken"))
-			putString("twitterAccessTokenSecret", twitterObj.getString("twitterAccessTokenSecret"))
-			commit()
+		try {
+			val twitterObj = jObj.getJSONObject("twitter")
+			sharedPreferences.edit {
+				putString("twitterAPIKey", twitterObj.getString("twitterAPIKey"))
+				putString("twitterAPIKeySecret", twitterObj.getString("twitterAPIKeySecret"))
+				putString("twitterAccessToken", twitterObj.getString("twitterAccessToken"))
+				putString("twitterAccessTokenSecret", twitterObj.getString("twitterAccessTokenSecret"))
+				commit()
+			}
+		} catch (e: Exception) {
 		}
 
-		val discordObj = jObj.getJSONObject("discord")
-		sharedPreferences.edit {
-			putBoolean("enableDiscordNotifications", discordObj.getBoolean("enableDiscordNotifications"))
-			putString("discordToken", discordObj.getString("discordToken"))
-			putString("discordUserID", discordObj.getString("discordUserID"))
-			commit()
+
+		try {
+			val discordObj = jObj.getJSONObject("discord")
+			sharedPreferences.edit {
+				putBoolean("enableDiscordNotifications", discordObj.getBoolean("enableDiscordNotifications"))
+				putString("discordToken", discordObj.getString("discordToken"))
+				putString("discordUserID", discordObj.getString("discordUserID"))
+				commit()
+			}
+		} catch (e: Exception) {
 		}
 
-		val configurationObj = jObj.getJSONObject("configuration")
-		sharedPreferences.edit {
-			putBoolean("enableAutoRestore", configurationObj.getBoolean("enableAutoRestore"))
-			putBoolean("enableFullElixir", configurationObj.getBoolean("enableFullElixir"))
-			putBoolean("enableSoulBalm", configurationObj.getBoolean("enableSoulBalm"))
-			putBoolean("enableDelayBetweenRuns", configurationObj.getBoolean("enableDelayBetweenRuns"))
-			putInt("delayBetweenRuns", configurationObj.getInt("delayBetweenRuns"))
-			putBoolean("enableRandomizedDelayBetweenRuns", configurationObj.getBoolean("enableRandomizedDelayBetweenRuns"))
-			putInt("delayBetweenRunsLowerBound", configurationObj.getInt("delayBetweenRunsLowerBound"))
-			putInt("delayBetweenRunsUpperBound", configurationObj.getInt("delayBetweenRunsUpperBound"))
-			putBoolean("enableRefreshDuringCombat", configurationObj.getBoolean("enableRefreshDuringCombat"))
-			putBoolean("enableAutoQuickSummon", configurationObj.getBoolean("enableAutoQuickSummon"))
-			putBoolean("enableBypassResetSummon", configurationObj.getBoolean("enableBypassResetSummon"))
-			commit()
+
+		try {
+			val configurationObj = jObj.getJSONObject("configuration")
+			sharedPreferences.edit {
+				putBoolean("enableAutoRestore", configurationObj.getBoolean("enableAutoRestore"))
+				putBoolean("enableFullElixir", configurationObj.getBoolean("enableFullElixir"))
+				putBoolean("enableSoulBalm", configurationObj.getBoolean("enableSoulBalm"))
+				putBoolean("enableDelayBetweenRuns", configurationObj.getBoolean("enableDelayBetweenRuns"))
+				putInt("delayBetweenRuns", configurationObj.getInt("delayBetweenRuns"))
+				putBoolean("enableRandomizedDelayBetweenRuns", configurationObj.getBoolean("enableRandomizedDelayBetweenRuns"))
+				putInt("delayBetweenRunsLowerBound", configurationObj.getInt("delayBetweenRunsLowerBound"))
+				putInt("delayBetweenRunsUpperBound", configurationObj.getInt("delayBetweenRunsUpperBound"))
+				putBoolean("enableRefreshDuringCombat", configurationObj.getBoolean("enableRefreshDuringCombat"))
+				putBoolean("enableAutoQuickSummon", configurationObj.getBoolean("enableAutoQuickSummon"))
+				putBoolean("enableBypassResetSummon", configurationObj.getBoolean("enableBypassResetSummon"))
+				commit()
+			}
+		} catch (e: Exception) {
 		}
 
-		val nightmareObj = jObj.getJSONObject("nightmare")
-		sharedPreferences.edit {
-			putBoolean("enableNightmare", nightmareObj.getBoolean("enableNightmare"))
-			putString("nightmareCombatScriptName", nightmareObj.getString("nightmareCombatScriptName"))
-			putString("nightmareCombatScript", toArrayList(nightmareObj.getJSONArray("nightmareCombatScript")).joinToString("|"))
-			putString("nightmareSummons", toArrayList(nightmareObj.getJSONArray("nightmareSummons")).joinToString("|"))
-			putString("nightmareSummonElements", toArrayList(nightmareObj.getJSONArray("nightmareSummonElements")).joinToString("|"))
-			putInt("nightmareGroupNumber", nightmareObj.getInt("nightmareGroupNumber"))
-			putInt("nightmarePartyNumber", nightmareObj.getInt("nightmarePartyNumber"))
-			commit()
+
+		try {
+			val nightmareObj = jObj.getJSONObject("nightmare")
+			sharedPreferences.edit {
+				putBoolean("enableNightmare", nightmareObj.getBoolean("enableNightmare"))
+				putString("nightmareCombatScriptName", nightmareObj.getString("nightmareCombatScriptName"))
+				putString("nightmareCombatScript", toArrayList(nightmareObj.getJSONArray("nightmareCombatScript")).joinToString("|"))
+				putString("nightmareSummons", toArrayList(nightmareObj.getJSONArray("nightmareSummons")).joinToString("|"))
+				putString("nightmareSummonElements", toArrayList(nightmareObj.getJSONArray("nightmareSummonElements")).joinToString("|"))
+				putInt("nightmareGroupNumber", nightmareObj.getInt("nightmareGroupNumber"))
+				putInt("nightmarePartyNumber", nightmareObj.getInt("nightmarePartyNumber"))
+				commit()
+			}
+		} catch (e: Exception) {
 		}
 
-		val eventObj = jObj.getJSONObject("event")
-		sharedPreferences.edit {
-			putBoolean("enableLocationIncrementByOne", eventObj.getBoolean("enableLocationIncrementByOne"))
-			commit()
+
+		try {
+			val eventObj = jObj.getJSONObject("event")
+			sharedPreferences.edit {
+				putBoolean("enableLocationIncrementByOne", eventObj.getBoolean("enableLocationIncrementByOne"))
+				commit()
+			}
+		} catch (e: Exception) {
 		}
 
-		val raidObj = jObj.getJSONObject("raid")
-		sharedPreferences.edit {
-			putBoolean("enableAutoExitRaid", raidObj.getBoolean("enableAutoExitRaid"))
-			putInt("timeAllowedUntilAutoExitRaid", raidObj.getInt("timeAllowedUntilAutoExitRaid"))
-			putBoolean("enableNoTimeout", raidObj.getBoolean("enableNoTimeout"))
-			commit()
+
+
+		try {
+			val raidObj = jObj.getJSONObject("raid")
+			sharedPreferences.edit {
+				putBoolean("enableAutoExitRaid", raidObj.getBoolean("enableAutoExitRaid"))
+				putInt("timeAllowedUntilAutoExitRaid", raidObj.getInt("timeAllowedUntilAutoExitRaid"))
+				putBoolean("enableNoTimeout", raidObj.getBoolean("enableNoTimeout"))
+				commit()
+			}
+		} catch (e: Exception) {
 		}
 
-		val arcarumObj = jObj.getJSONObject("arcarum")
-		sharedPreferences.edit {
-			putBoolean("enableStopOnArcarumBoss", arcarumObj.getBoolean("enableStopOnArcarumBoss"))
-			commit()
+
+		try {
+			val arcarumObj = jObj.getJSONObject("arcarum")
+			sharedPreferences.edit {
+				putBoolean("enableStopOnArcarumBoss", arcarumObj.getBoolean("enableStopOnArcarumBoss"))
+				commit()
+			}
+		} catch (e: Exception) {
 		}
 
-		val genericObj = jObj.getJSONObject("generic")
-		sharedPreferences.edit {
-			putBoolean("enableForceReload", genericObj.getBoolean("enableForceReload"))
-			commit()
+
+		try {
+			val genericObj = jObj.getJSONObject("generic")
+			sharedPreferences.edit {
+				putBoolean("enableForceReload", genericObj.getBoolean("enableForceReload"))
+				commit()
+			}
+		} catch (e: Exception) {
 		}
 
-		val adjustmentObj = jObj.getJSONObject("adjustment")
-		sharedPreferences.edit {
-			putBoolean("enableCalibrationAdjustment", adjustmentObj.getBoolean("enableCalibrationAdjustment"))
-			putInt("adjustCalibration", adjustmentObj.getInt("adjustCalibration"))
-			putBoolean("enableGeneralAdjustment", adjustmentObj.getBoolean("enableGeneralAdjustment"))
-			putInt("adjustButtonSearchGeneral", adjustmentObj.getInt("adjustButtonSearchGeneral"))
-			putInt("adjustHeaderSearchGeneral", adjustmentObj.getInt("adjustHeaderSearchGeneral"))
-			putBoolean("enableSupportSummonSelectionScreenAdjustment", adjustmentObj.getBoolean("enableSupportSummonSelectionScreenAdjustment"))
-			putInt("adjustSupportSummonSelectionScreen", adjustmentObj.getInt("adjustSupportSummonSelectionScreen"))
-			putBoolean("enableCombatModeAdjustment", adjustmentObj.getBoolean("enableCombatModeAdjustment"))
-			putInt("adjustCombatStart", adjustmentObj.getInt("adjustCombatStart"))
-			putInt("adjustSkillUsage", adjustmentObj.getInt("adjustSkillUsage"))
-			putInt("adjustSummonUsage", adjustmentObj.getInt("adjustSummonUsage"))
-			putInt("adjustWaitingForReload", adjustmentObj.getInt("adjustWaitingForReload"))
-			putInt("adjustWaitingForAttack", adjustmentObj.getInt("adjustWaitingForAttack"))
-			commit()
+
+		try {
+			val adjustmentObj = jObj.getJSONObject("adjustment")
+			sharedPreferences.edit {
+				putBoolean("enableCalibrationAdjustment", adjustmentObj.getBoolean("enableCalibrationAdjustment"))
+				putInt("adjustCalibration", adjustmentObj.getInt("adjustCalibration"))
+				putBoolean("enableGeneralAdjustment", adjustmentObj.getBoolean("enableGeneralAdjustment"))
+				putInt("adjustButtonSearchGeneral", adjustmentObj.getInt("adjustButtonSearchGeneral"))
+				putInt("adjustHeaderSearchGeneral", adjustmentObj.getInt("adjustHeaderSearchGeneral"))
+				putBoolean("enableSupportSummonSelectionScreenAdjustment", adjustmentObj.getBoolean("enableSupportSummonSelectionScreenAdjustment"))
+				putInt("adjustSupportSummonSelectionScreen", adjustmentObj.getInt("adjustSupportSummonSelectionScreen"))
+				putBoolean("enableCombatModeAdjustment", adjustmentObj.getBoolean("enableCombatModeAdjustment"))
+				putInt("adjustCombatStart", adjustmentObj.getInt("adjustCombatStart"))
+				putInt("adjustSkillUsage", adjustmentObj.getInt("adjustSkillUsage"))
+				putInt("adjustSummonUsage", adjustmentObj.getInt("adjustSummonUsage"))
+				putInt("adjustWaitingForReload", adjustmentObj.getInt("adjustWaitingForReload"))
+				putInt("adjustWaitingForAttack", adjustmentObj.getInt("adjustWaitingForAttack"))
+				commit()
+			}
+		} catch (e: Exception) {
 		}
 
-		val androidObj = jObj.getJSONObject("android")
-		sharedPreferences.edit {
-			putBoolean("enableDelayTap", androidObj.getBoolean("enableDelayTap"))
-			putInt("delayTapMilliseconds", androidObj.getInt("delayTapMilliseconds"))
-			putFloat("confidence", androidObj.getDouble("confidence").toFloat())
-			putFloat("confidenceAll", androidObj.getDouble("confidenceAll").toFloat())
-			putFloat("customScale", androidObj.getDouble("customScale").toFloat())
-			putBoolean("enableTestForHomeScreen", androidObj.getBoolean("enableTestForHomeScreen"))
-			commit()
+
+
+		try {
+			val androidObj = jObj.getJSONObject("android")
+			sharedPreferences.edit {
+				putBoolean("enableDelayTap", androidObj.getBoolean("enableDelayTap"))
+				putInt("delayTapMilliseconds", androidObj.getInt("delayTapMilliseconds"))
+				putFloat("confidence", androidObj.getDouble("confidence").toFloat())
+				putFloat("confidenceAll", androidObj.getDouble("confidenceAll").toFloat())
+				putFloat("customScale", androidObj.getDouble("customScale").toFloat())
+				putBoolean("enableTestForHomeScreen", androidObj.getBoolean("enableTestForHomeScreen"))
+				commit()
+			}
+		} catch (e: Exception) {
 		}
+
 
 		Log.d(tag, "Successfully loaded settings into SharedPreferences.")
 	}
