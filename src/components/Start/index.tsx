4b4d09e6d1ac03f9772bcecee3e25341815957cf
Steve Tu <stevetu11@yahoo.com>
2022-07-01 19:46:35 -0700

    Add reworked listener for the message log and add logic for API calling

--- src/components/Start/index.tsx
+++ src/components/Start/index.tsx
@@ -2,16 +2,54 @@ import RNFS from "react-native-fs"
 import { BotStateContext, defaultSettings, Settings } from "../../context/BotStateContext"
 import { MessageLogContext } from "../../context/MessageLogContext"
 import { useContext, useEffect, useState } from "react"
+import axios, { AxiosError, AxiosResponse } from "axios"
+import { DeviceEventEmitter } from "react-native"
 
 const Start = () => {
     const [firstTime, setFirstTime] = useState<boolean>(true)
+    const [firstTimeAPIRequest, setFirstTimeAPIRequest] = useState<boolean>(true)
+    const [apiData, setAPIData] = useState<APIData>({ itemName: "", amount: 1, elapsedTime: "" })
+    const [sendAPIRequestNow, setSendAPIRequestNow] = useState<boolean>(false)
+    const [successfulAPILogin, setSuccessfulAPILogin] = useState<boolean>(true)
 
     const bsc = useContext(BotStateContext)
     const mlc = useContext(MessageLogContext)
 
+    interface APIData {
+        itemName: string
+        amount: number
+        elapsedTime: string
+    }
+
     // Load settings if local settings.json file exists in internal storage.
     useEffect(() => {
         loadSettings()
+
+        // Add listener for API specific logic.
+        DeviceEventEmitter.addListener("MessageLog", (data) => {
+            if (successfulAPILogin && data["message"].includes("API-RESULT")) {
+                // Format of the line is API-RESULT|ITEM NAME|ITEM AMOUNT|TIME
+                let newLog: string[] = []
+                let line = data["message"]
+                const splitLine = line.split("|")
+                if (splitLine.length !== 4) {
+                    console.log(`Unable to send API request to Granblue Automation Statistics: Invalid request format of ${splitLine.length}.`)
+                    newLog = [...mlc.asyncMessages, `\nUnable to send API request to Granblue Automation Statistics: Invalid request format of ${splitLine.length}.`]
+                } else if (Number.isNaN(parseInt(splitLine[2]))) {
+                    console.log(`Unable to send API request to Granblue Automation Statistics: Invalid type for item amount.`)
+                    newLog = [...mlc.asyncMessages, `\nUnable to send API request to Granblue Automation Statistics: Invalid type for item amount.`]
+                } else {
+                    setAPIData({ itemName: splitLine[1], amount: parseInt(splitLine[2]), elapsedTime: splitLine[3] })
+                    setSendAPIRequestNow(true)
+                }
+
+                newLog = [...newLog, `\n${line}`]
+                mlc.setAsyncMessages(newLog)
+            } else {
+                let newLog = [...mlc.asyncMessages, `\n${data["message"]}`]
+                mlc.setAsyncMessages(newLog)
+            }
+        })
     }, [])
 
     useEffect(() => {
@@ -145,6 +183,133 @@ const Start = () => {
             bsc.setReadyStatus(bsc.settings.game.item !== "" && bsc.settings.game.mission !== "")
         } else {
             bsc.setReadyStatus(farmingMode === "Generic" && bsc.settings.game.item !== "" && bsc.settings.game.summons.length !== 0)
+        }
+
+        handleStart()
+    }
+
+    const handleStart = async () => {
+        // Login to API.
+        if (bsc.settings.api.enableOptInAPI) {
+            await loginToAPI()
+        }
+    }
+
+    // Login to the Granblue Automation Statistics API.
+    const loginToAPI = async () => {
+        await axios
+            .post(
+                `${bsc.entryPoint}/api/login`,
+                {
+                    username: bsc.settings.api.username,
+                    password: bsc.settings.api.password,
+                },
+                {
+                    withCredentials: true,
+                }
+            )
+            .then(() => {
+                console.log("I AM LOGGED IN")
+                let newLog = [...mlc.asyncMessages, `Successfully logged into Granblue Automation Statistics API.\n`]
+                mlc.setAsyncMessages(newLog)
+                setSuccessfulAPILogin(true)
+                setFirstTimeAPIRequest(true)
+            })
+            .catch((e) => {
+                let newLog = [...mlc.asyncMessages, `Failed to login to Granblue Automation Statistics API: ${e}\n`]
+                mlc.setAsyncMessages(newLog)
+            })
+    }
+
+    useEffect(() => {
+        if (sendAPIRequestNow) {
+            sendAPIRequest(apiData.itemName, apiData.amount, apiData.elapsedTime)
+            setSendAPIRequestNow(false)
+        }
+    }, [sendAPIRequestNow])
+
+    // Send a API request to create a new result in the database.
+    const sendAPIRequest = async (itemName: string, amount: number, elapsedTime: string) => {
+        // If this is the first time, create the item if it does not already exist in the database.
+        let newLog: string[] = []
+        if (firstTimeAPIRequest) {
+            const data = {
+                username: bsc.settings.api.username,
+                password: bsc.settings.api.password,
+                farmingMode: bsc.settings.game.farmingMode,
+                mission: bsc.settings.game.mission,
+                itemName: itemName,
+            }
+
+            console.log("Data: ", data)
+
+            await axios
+                .post(`${bsc.entryPoint}/api/create-item`, data, { withCredentials: true })
+                .then(async (res: AxiosResponse) => {
+                    console.log("[API] ", res.data)
+                    newLog = [...mlc.asyncMessages, `\n[API] ${res.data}`]
+                    setFirstTimeAPIRequest(false)
+                    await axios
+                        .post(
+                            `${bsc.entryPoint}/api/create-result`,
+                            {
+                                username: bsc.settings.api.username,
+                                password: bsc.settings.api.password,
+                                farmingMode: bsc.settings.game.farmingMode,
+                                mission: bsc.settings.game.mission,
+                                itemName: itemName,
+                                platform: "GAA",
+                                amount: amount,
+                                elapsedTime: elapsedTime,
+                                appVersion: bsc.appVersion,
+                            },
+                            { withCredentials: true }
+                        )
+                        .then((res: AxiosResponse) => {
+                            console.log("[API] ", res.data)
+                            newLog = [...newLog, `\n[API] ${res.data}`]
+                        })
+                        .catch((e) => {
+                            newLog = [...newLog, `\n[API] Failed to create result: ${e?.response?.data}`]
+                        })
+                        .finally(() => {
+                            mlc.setAsyncMessages(newLog)
+                        })
+                })
+                .catch((e: AxiosError) => {
+                    console.error(`[API] Failed to create item for the first time: `, e.response?.request["_response"])
+                    newLog = [...mlc.asyncMessages, `\n[API] Failed to create item for the first time: ${e.response?.request["_response"]}`]
+                    mlc.setAsyncMessages(newLog)
+                })
+        } else {
+            console.log("I AM MAKING A SECOND REQUEST")
+            let newLog: string[] = []
+            await axios
+                .post(
+                    `${bsc.entryPoint}/api/create-result`,
+                    {
+                        username: bsc.settings.api.username,
+                        password: bsc.settings.api.password,
+                        farmingMode: bsc.settings.game.farmingMode,
+                        mission: bsc.settings.game.mission,
+                        itemName: itemName,
+                        platform: "GAA",
+                        amount: amount,
+                        elapsedTime: elapsedTime,
+                        appVersion: bsc.appVersion,
+                    },
+                    { withCredentials: true }
+                )
+                .then((res: AxiosResponse) => {
+                    console.log("[API] ", res.data)
+                    newLog = [...mlc.asyncMessages, `\n[API] ${res.data} || ${bsc.settings.game.mission}`]
+                })
+                .catch((e: AxiosError) => {
+                    newLog = [...mlc.asyncMessages, `\n[API] Failed to create result: ${e.response?.request["_response"]}`]
+                })
+                .finally(() => {
+                    mlc.setAsyncMessages(newLog)
+                })
         }
     }
 
